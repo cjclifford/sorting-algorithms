494ac6df375e475576afb9c12cabd146702e8526
Cuan <cliffordcuan@gmail.com>
2019-09-19 14:00:51 +0000

    adds insertion sort algorithm with performance test

--- sort.py
+++ sort.py
@@ -38,3 +38,34 @@ end_time = time.perf_counter_ns()
 print('Bubble Sort')
 print(f'Time: {end_time - start_time:,}ns')
 print()
+
+# Insertion Sort
+#
+# Iterates through each element in a list, continuously swapping with it's
+# previous value if current value is smaller than the prior.
+#
+# Data structure: Array
+#
+# Time complexity:
+#   Worst:    O(n^2) comparisons and swaps
+#   Best:     O(n) comparisons and swaps
+#   Average:  O(n^2) comparisons and swaps
+#
+# Worst-case space complexity:
+#   Total:      O(n)
+#   Auxillary:  O(1)
+def insertion_sort(lst):
+  for i in range(len(lst)):
+    j = i
+    while j > 0 and lst[j - 1] > lst[j]:
+      lst[j], lst[j - 1] = lst[j - 1], lst[j]
+      j = j - 1
+  return lst
+
+start_time = time.perf_counter_ns()
+sorted = insertion_sort(gen_list[:])
+end_time = time.perf_counter_ns()
+
+print('Insertion Sort')
+print(f'Time: {end_time - start_time:,}ns')
+print()
